583524e6d04290f71610a60b84bd99c9d42a04fa
Ignat Bavtovich <ignatbav@gmail.com>
2019-01-12 19:58:44 +0300

    Upload file with new

--- routes/photos.js
+++ routes/photos.js
@@ -5,8 +5,9 @@ var fs = require('fs');
 
 router.put('/:id', function (req, res) {
 	let emptyFieldsInReq = !Object.keys(req.body).length;
+	let justOneFileInReq = !Array.isArray(photo);
 	let photo = req.files.photo;
-	if (req.files.photo && !Array.isArray(photo) && emptyFieldsInReq) {
+	if (req.files.photo && justOneFileInReq && emptyFieldsInReq) {
 		updatePhoto(req.params.id, fs.readFileSync(photo.file,'base64'));
 		res.status(200).json({'status': 'Photo was successfully updated'});
 	} else {
