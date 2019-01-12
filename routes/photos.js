0bf59e1f5ec2f247f170e90ce9d079dde564712c
Ignat Bavtovich <ignatbav@gmail.com>
2019-01-12 19:22:30 +0300

    Improved photo uploading using busboy package

--- routes/photos.js
+++ routes/photos.js
@@ -1,13 +1,39 @@
 var express = require('express');
+var Busboy = require('busboy');
 var router = express.Router();
 var updatePhoto = require('../services/database');
+var concat = require('concat-stream')
 
 
-router.put('/:id', function(req, res) {
-  console.log("try to upload photo");
-  var updatePhoto1 = updatePhoto(req.params.id, req.body);
-  console.log(updatePhoto1);
-  res.end();
+router.put('/:id', function (req, res) {
+	var busboy = new Busboy({headers: req.headers});
+
+	busboy.on('file', function (filename, file) {
+		if (!res._headerSent && filename === 'photo') {
+			file.pipe(concat(function (fileBuffer) {
+				updatePhoto(req.params.id, fileBuffer.toString('base64'));
+			}));
+
+		} else {
+			if (!res._headerSent) {
+				res.status(500).json({'Error': 'Invalid data format'})
+			}
+		}
+	});
+
+	busboy.on('field', function () {
+		if (!res._headerSent) {
+			res.status(500).json({'Error': 'Invalid data format'})
+		}
+	});
+
+	busboy.on('finish', function () {
+		if (!res._headerSent) {
+			res.status(200).json({'status': 'Photo was successfully updated'});
+		}
+	});
+
+	req.pipe(busboy);
 });
 
 module.exports = router;
--- services/database.js
+++ services/database.js
@@ -6,10 +6,11 @@ const adapter = new FileSync('users.json');
 const db = low(adapter);
 
 var updateImage = function updateImage(id, photo) {
-    console.log(photo);
-    db.find({id: id}).photo = photo;
-    db.save();
-
+    db.get('users')
+        .find({id: id})
+        .value().photo = photo;
+    db.write();
+    console.log("Photo was updated for user with id: " + id);
 };
 
 module.exports = updateImage;
